2788699235b38142a97981360fb02be9b81e8d8f
Saminsfw <dev.saminsfw@gmail.com>
2023-09-26 10:51:38 +0530

    add border-radius to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,13 @@ const config: Config = {
     './stories/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    borderRadius:{
+          5:'5px',
+          8:'8px',
+          16:'16px',
+          30:'30px',
+          full:'100%',
+        },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -16,22 +23,11 @@ const config: Config = {
       },
       colors: {
         darkBlue : '#032541',
-
-
         darkGrey : '#757575 ',
-
-
         purple :' #805BE7',
-
         lightBlue : '#01B4E4',
-
-
         lightBrown : '#897474',
-
-
         grey : '#D7D7D7',
-
-
         lightGrey: 'E4E4E4',
       }
     },
